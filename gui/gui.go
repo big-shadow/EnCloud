7d4627647b2cd9d9bd0da0dcbf211f73d75ccc8a
Ray Winkelman <raywinkelman@gmail.com>
2015-11-07 13:22:41 -0400

    Added go-gtk lib and messed around.

---
+++ gui/gui.go
@@ -0,0 +1,44 @@
+package gui
+
+import (
+	"os"
+	"strconv"
+
+	"EnCloud/go-gtk/gtk"
+)
+
+func BuildForm() {
+	gtk.Init(&os.Args)
+	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	window.SetTitle("EnCloud - Cloud Encryption")
+	window.Connect("destroy", gtk.MainQuit)
+
+	notebook := gtk.NewNotebook()
+
+	for n := 1; n <= 10; n++ {
+		page := gtk.NewFrame("demo" + strconv.Itoa(n))
+		notebook.AppendPage(page, gtk.NewLabel("demo"+strconv.Itoa(n)))
+
+		vbox := gtk.NewHBox(false, 1)
+
+		prev := gtk.NewButtonWithLabel("go prev")
+		prev.Clicked(func() {
+			notebook.PrevPage()
+		})
+		vbox.Add(prev)
+
+		next := gtk.NewButtonWithLabel("go next")
+		next.Clicked(func() {
+			notebook.NextPage()
+		})
+		vbox.Add(next)
+
+		page.Add(vbox)
+	}
+
+	window.Add(notebook)
+	window.SetSizeRequest(400, 200)
+	window.ShowAll()
+
+	gtk.Main()
+}
